75f60bacb22d35ccc2f0ca1e40c1292b058020b6
grace ohalloran <grace@advancinganalytics.co.uk>
2021-12-14 09:59:49 +0000

    day 10 part 2

--- Day10/Python/RogueSquadron-SyntaxScoring.py
+++ Day10/Python/RogueSquadron-SyntaxScoring.py
@@ -11,19 +11,20 @@ file.close()
 
 
 # Part 1
-        
+illegalLines = []    
 illegalChars = []
+j = 0
 for line in lines:   
     openList = []
     closedList = []
     i=0
     for c in line:
-
         if c == ')':
             if openList[-1] == '(':
                 openList = openList[:-1]
             else:
                 illegalChars.append(line[i])
+                illegalLines.append(j)
                 break
             closedList.append(c)
         elif c == '(':
@@ -33,6 +34,7 @@ for line in lines:
                 openList = openList[:-1]
             else:
                 illegalChars.append(line[i])
+                illegalLines.append(j)
                 break
             closedList.append(c)
         elif c == '[':
@@ -42,6 +44,7 @@ for line in lines:
                 openList = openList[:-1]
             else:
                 illegalChars.append(line[i])
+                illegalLines.append(j)
                 break
             closedList.append(c)
         elif c == '{':
@@ -51,6 +54,7 @@ for line in lines:
                 openList = openList[:-1]
             else:
                 illegalChars.append(line[i])
+                illegalLines.append(j)
                 break
             closedList.append(c)
         elif c == '<':
@@ -58,6 +62,7 @@ for line in lines:
         else:
             pass
         i+=1
+    j+=1
 
 rbPoints = 3
 sbPoints = 57
@@ -80,4 +85,95 @@ for c in illegalChars:
     i+=1
     
 score = sum(illegalChars)
-print('Score: ' + str(score))
+print('Part 1 Score: ' + str(score))
+
+# Part 2
+
+linesPart2 = lines
+
+illegalLinesRev = illegalLines[::-1]
+for l in illegalLinesRev:
+    del linesPart2[l]
+    
+    
+j = 0
+completionStrings = []
+completionLists = []
+for line in linesPart2:   
+    i=0
+    openList = ['0']
+    for c in line:
+        if c == ')':
+            if openList[-1] == '(':
+                openList = openList[:-1]
+            else:
+                pass
+        elif c == '(':
+            openList.append(c)
+        elif c == ']':
+            if openList[-1] == '[':
+                openList = openList[:-1]
+            else:
+                pass
+        elif c == '[':
+            openList.append(c)
+        elif c == '}':
+            if openList[-1] == '{':
+                openList = openList[:-1]
+            else:
+                pass
+        elif c == '{':
+            openList.append(c)
+        elif c == '>':
+            if openList[-1] == '<':
+                openList = openList[:-1]
+            else:
+                pass
+        elif c == '<':
+            openList.append(c)
+        else:
+            pass
+        i+=1
+    openListTotal = openList[1:]
+    completeList = []
+    for elem in openListTotal:
+        if elem == '(':
+            completeList.append(')')
+        elif elem == '[':
+            completeList.append(']')
+        elif elem == '{':
+            completeList.append('}')
+        else:
+            completeList.append('>')
+    completeString = ''.join(completeList)
+    completionStrings.append(completeString[::-1])
+    completionLists.append(completeList[::-1])
+    j+=1
+
+rbScore = 1
+sbScore = 2
+cbScore = 3
+chScore = 4
+scores = []
+for lst in completionLists:
+    score = 0
+    for elem in lst:
+        score = score*5
+        if elem == ')':
+            score += rbScore
+        elif elem == ']':
+            score += sbScore
+        elif elem == '}':
+            score += cbScore
+        elif elem == '>':
+            score += chScore
+        else:
+            pass
+    scores.append(score)
+    
+sortedList = sorted(scores)
+midIndex = int((len(sortedList)/2)-0.5)
+
+finalScore = sortedList[midIndex]
+
+print('Part 2 Score: ' + str(finalScore))
